3a797da40b008e451461f748d6b024bd722e06d3
pippopil <voood234@gmail.com>
2022-01-24 17:13:25 +0300

    Add

---
+++ src/test/java/ru/netology/manager/ProductManagerTest.java
@@ -0,0 +1,100 @@
+package ru.netology.manager;
+
+import org.junit.jupiter.api.Test;
+import ru.netology.domain.Book;
+import ru.netology.domain.Product;
+import ru.netology.domain.Smartphone;
+import ru.netology.repository.ProductRepository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductManagerTest {
+
+    private ProductRepository repository = new ProductRepository();
+    private ProductManager manager = new ProductManager(repository);
+
+    Product first = new Book(1, "Война и Мир", 1000, "Толстой");
+    Product second = new Book(2, "Отрочество", 1100, "Толстой");
+    Product third = new Book(3, "Прошу,убей меня!", 900, "Маккейн");
+    Product fourth = new Smartphone(4, "Iphone", 400, "Apple");
+    Product fifth = new Smartphone(5, "A70", 300, "Samsung");
+
+
+    public void setUp() {
+        manager.add(first);
+        manager.add(second);
+        manager.add(third);
+        manager.add(fourth);
+        manager.add(fifth);
+    }
+    @Test
+    public void addProduct() {
+        manager.add(first);
+        assertArrayEquals(new Product[]{first}, repository.getAll());
+    }
+
+    @Test
+    void searchByName() {
+        setUp();
+
+        Product[] expected = new Product[]{third};
+        Product[] actual = manager.searchBy("Прошу,убей меня!");
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void searchMatchesNameBook() {
+        setUp();
+
+        Product[] expected = new Product[]{second};
+        Product[] actual = manager.searchBy("Отрочество");
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void searchMatchesAuthor() {
+        setUp();
+
+        Product[] expected = new Product[]{third};
+        Product[] actual = manager.searchBy("Маккейн");
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void searchMatchesManufacture() {
+        setUp();
+
+        Product[] expected = new Product[]{fifth};
+        Product[] actual = manager.searchBy("Samsung");
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void searchMatchesNameSmartphone() {
+        setUp();
+
+        Product[] expected = new Product[]{fourth};
+        Product[] actual = manager.searchBy("Iphone");
+        assertArrayEquals(expected, actual);
+    }
+
+    //    поиск всех книг одного автора
+    @Test
+    void searchAllByAuthor() {
+        setUp();
+
+        Product[] expected = new Product[]{second, first};
+        Product[] actual = manager.searchBy("Толстой");
+        assertArrayEquals(expected, actual);
+    }
+    //    Запрос, на который нет ответа
+    @Test
+    void searchAll() {
+        setUp();
+
+        Product[] expected = new Product[]{};
+        Product[] actual = manager.searchBy(null);
+        assertArrayEquals(expected, actual);
+    }
+
+}
---
+++ src/test/java/ru/netology/repository/ProductRepositoryTest.java
@@ -0,0 +1,41 @@
+package ru.netology.repository;
+
+import org.junit.jupiter.api.Test;
+import ru.netology.domain.Book;
+import ru.netology.domain.Product;
+
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ProductRepositoryTest {
+    private ProductRepository repository = new ProductRepository();
+    private Book coreJava = new Book();
+    Product first = new Book(1, "Война и Мир", 1000, "Толстой");
+    Product second = new Book(2, "Отрочество", 1100, "Толстой");
+
+    @Test
+    public void shouldSaveOneItem() {
+        repository.save(coreJava);
+
+        Product[] expected = new Product[]{coreJava};
+        Product[] actual = repository.getAll();
+
+        assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void shouldRemoveIfExists() {
+        repository.save(first);
+
+        int idToRemove=1;
+        repository.removeById(idToRemove);
+
+        Product[] expected = new Product[] {};
+        Product[] actual = repository.getAll();
+
+        assertArrayEquals(expected, actual);
+
+    }
+
+
+}
